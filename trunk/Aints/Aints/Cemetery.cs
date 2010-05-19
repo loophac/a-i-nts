7d06b8713e27b6b427c3bbb9195995e46271fa3e
vandonr <vandonr@b350ded3-09b7-b03a-bf75-46ddbb29fafc>
2010-05-19 21:25:47 +0000

    cemetery implementation

--- trunk/Aints/Aints/Cemetery.cs
+++ trunk/Aints/Aints/Cemetery.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Aints
 {
 	public class Cemetery : List<Ant>
 	{
+		public readonly Vector2 Position;
 
+		public Cemetery(Ant a)
+			: base()
+		{
+			this.Add(a);
+			this.Position = a.Position;
+		}
 	}
 }
